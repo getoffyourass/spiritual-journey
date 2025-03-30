566e47c9cd012c4c87dcda5ead599f5b939106e2
getoffyourass <rackcitybih7@gmail.com>
2025-03-30 05:07:46 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="app">
+            <h1>Welcome to Spiritual Journey</h1>
+            <p>Your path to inner peace begins here.</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
